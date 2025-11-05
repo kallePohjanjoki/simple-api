2401aef8696e4e183825ea6d54c99d326c2f7646
Kalle Pohjanjoki <kalle@pohjanjoki.com>
2025-11-05 19:54:40 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+// Määritä portti. 3000 on yleinen oletus Node.js-sovelluksille.
+const PORT = 3000;
+
+// Määritä reitti (route)
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Tervehdys pilvestä!',
+    timestamp: new Date().toISOString()
+  });
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Palvelin kuuntelee porttia ${PORT}`);
+});
